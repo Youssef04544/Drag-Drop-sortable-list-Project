fc341ca27e7f198cd836b6d41af237835449c388
Youssef04544 <youssefbenyedder04@gmail.com>
2021-07-24 09:04:06 +0100

    Added the check for closest element function to determine where to insert the dragged element

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ draggables.forEach(draggable => {
     draggable.classList.remove("dragging");
   })
 })
+
+containers.forEach(container => {
+  container.addEventListener("dragover", (e) => {
+    const dragging = document.querySelector (".dragging"); //Get's the only element being currently dragged
+    const afterEement = getAfterElement(container, e.clientY); // Find the Element that is just below our dragged Element
+    container.appendChild(dragging);
+  })
+})
+
+
+function getAfterElement(container, Y) {
+  const elements = [...container.querySelectorAll(".draggable:not(.dragging)")];
+  
+  return elements.reduce((closest,element) => {
+    const box = element.getBoundingClientRect();
+    const offset = Y - box.top - box.height / 2;
+
+    //checks if the element is below the current dragged one with offset < 0 and checks if it's closer than the closest element
+    if(offset < 0 && offset > closest.offset){
+      return closest = {offset, element};
+    }else{
+      return closest;
+    }
+  }, {offset: Number.NEGATIVE_INFINITY}).element;
+}
